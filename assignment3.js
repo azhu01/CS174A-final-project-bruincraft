5caa274596efe211bd9b5c721db05c46176bc904
Alvyn Wang <alvyn.wang@gmail.com>
2021-11-13 11:18:39 -0800

    Implement vertical look around

--- assignment3.js
+++ assignment3.js
@@ -19,7 +19,8 @@ export class Assignment3 extends Scene {
             circle: new defs.Regular_2D_Polygon(1, 15),
             // TODO:  Fill in as many additional shape instances as needed in this key/value table.
             //        (Requirement 1)
-            block: new defs.Cube(1),
+            block: new defs.Cube(),
+            floor: new defs.Square(),
         };
 
         // *** Materials
@@ -30,8 +31,8 @@ export class Assignment3 extends Scene {
             // TODO:  Fill in as many additional material objects as needed in this key/value table.
             //        (Requirement 4)
         }
-        this.position = vec3(0, 6, 20);
-        this.look_at = vec3(0,0,0);
+        this.position = vec3(0, 3, 20);
+        this.look_at = vec3(0,3,1);
         this.top = vec3(0,1,0);
         this.current_camera_location = Mat4.look_at(vec3(0, 6, 20), vec3(0, 0, 0), vec3(0, 1, 0));
         this.blocks = [];
@@ -40,7 +41,7 @@ export class Assignment3 extends Scene {
         for(let i = 0; i < numberOfSquares; i++) {
             //console.log(model_transform);
             for (let j = 0; j < numberOfSquares; j++) {
-                this.blocks.push([i * 2, 0, j * 2]);
+                //this.blocks.push([i * 2, 0, j * 2]);
                 if (Math.random() < 0.05) {
                     let height = Math.floor(Math.random() * 10);
                     for (let k = 0; k < height; k++) {
@@ -54,23 +55,14 @@ export class Assignment3 extends Scene {
 
     make_control_panel() {
         // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-        this.key_triggered_button("View solar system", ["Control", "0"], () => this.attached = () => null);
-        this.new_line();
-        this.key_triggered_button("Attach to planet 1", ["Control", "1"], () => this.attached = () => this.planet_1);
-        this.key_triggered_button("Attach to planet 2", ["Control", "2"], () => this.attached = () => this.planet_2);
-        this.new_line();
-        this.key_triggered_button("Attach to planet 3", ["Control", "3"], () => this.attached = () => this.planet_3);
-        this.key_triggered_button("Attach to planet 4", ["Control", "4"], () => this.attached = () => this.planet_4);
-        this.new_line();
-        this.key_triggered_button("Attach to moon", ["Control", "m"], () => this.attached = () => this.moon);
         this.key_triggered_button("Forward", ["w"], () => this.thrust[2] = 1, undefined, () => this.thrust[2] = 0);
         this.key_triggered_button("Backward", ["s"], () => this.thrust[3] = 1, undefined, () => this.thrust[3] = 0);
         this.key_triggered_button("Left", ["a"], () => this.thrust[0] = 1, undefined, () => this.thrust[0] = 0);
         this.key_triggered_button("Right", ["d"], () => this.thrust[1] = 1, undefined, () => this.thrust[1] = 0);
-        this.key_triggered_button("Look up", ["u"], () => this.direction[2] = 1, undefined, () => this.direction[2] = 0);
-        this.key_triggered_button("Look down", ["j"], () => this.direction[3] = 1, undefined, () => this.direction[3] = 0);
-        this.key_triggered_button("Look right", ["k"], () => this.direction[1] = 1, undefined, () => this.direction[1] = 0);
-        this.key_triggered_button("Look left", ["h"], () => this.direction[0] = 1, undefined, () => this.direction[0] = 0);
+        this.key_triggered_button("Look up", ["i"], () => this.direction[2] = 1, undefined, () => this.direction[2] = 0);
+        this.key_triggered_button("Look down", ["k"], () => this.direction[3] = 1, undefined, () => this.direction[3] = 0);
+        this.key_triggered_button("Look right", ["l"], () => this.direction[1] = 1, undefined, () => this.direction[1] = 0);
+        this.key_triggered_button("Look left", ["j"], () => this.direction[0] = 1, undefined, () => this.direction[0] = 0);
     }
 
     display(context, program_state) {
@@ -84,7 +76,7 @@ export class Assignment3 extends Scene {
         if (this.thrust[2] == 1){
             let look_direction = this.look_at.minus(this.position);
             look_direction = vec3(look_direction[0], 0, look_direction[2]).normalized();
-            console.log(look_direction)
+            //console.log(look_direction)
             this.position = this.position.plus(look_direction.times(0.5));
             this.look_at = this.look_at.plus(look_direction.times(0.5));
 
@@ -95,7 +87,7 @@ export class Assignment3 extends Scene {
         if (this.thrust[3] == 1){
             let look_direction = this.look_at.minus(this.position);
             look_direction = vec3(look_direction[0], 0, look_direction[2]).normalized();
-            console.log(look_direction)
+            //console.log(look_direction)
             this.position = this.position.minus(look_direction.times(0.5));
             this.look_at = this.look_at.minus(look_direction.times(0.5));
 
@@ -105,58 +97,86 @@ export class Assignment3 extends Scene {
             let right = look_direction.cross(vec3(0,1,0)).normalized();
             this.position = this.position.plus(right.times(0.2));
             this.look_at = this.look_at.plus(right.times(0.2));
-            console.log(this.look_at);
+            //console.log(this.look_at);
         }
-        if (this.thrust[0] == 1){
+        if (this.thrust[0] == 1) {
             let look_direction = this.look_at.minus(this.position);
             let right = look_direction.cross(vec3(0,1,0)).normalized();
             this.position = this.position.minus(right.times(0.2));
             this.look_at = this.look_at.minus(right.times(0.2));
         }
-        if (this.direction[2] == 1){
-            this.look_at = vec3(this.look_at[0], this.look_at[1]+0.05, this.look_at[2]);
-            console.log(this.look_at);
+        if (this.direction[2] == 1) {
+            let vec4LookAt = vec4(this.look_at[0], this.look_at[1], this.look_at[2], 1);
+            let vec4Top = vec4(this.top[0], this.top[1], this.top[2], 1);
+            let look_direction = this.look_at.minus(this.position);
+            let rotationAxis = look_direction.cross(this.top).normalized();
+            let model_transform = Mat4.identity();
+            model_transform = model_transform.times(Mat4.translation(this.position[0], this.position[1], this.position[2]));
+            model_transform = model_transform.times(Mat4.rotation(0.01, rotationAxis[0], rotationAxis[1], rotationAxis[2]));
+            model_transform = model_transform.times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
+            vec4LookAt = model_transform.times(vec4LookAt);
+            vec4Top = Mat4.rotation(0.01, rotationAxis[0], rotationAxis[1], rotationAxis[2]).times(vec4Top);
+
+            this.look_at = vec3(vec4LookAt[0], vec4LookAt[1], vec4LookAt[2]);
+            this.top = vec3(vec4Top[0], vec4Top[1], vec4Top[2]);
+
+            //this.look_at = vec3(this.look_at[0], this.look_at[1]+0.05, this.look_at[2]);
+            //console.log(this.look_at);
         }
         if (this.direction[3] == 1){
-            this.look_at = vec3(this.look_at[0], this.look_at[1]-0.05, this.look_at[2]);
+            let vec4LookAt = vec4(this.look_at[0], this.look_at[1], this.look_at[2], 1);
+            let vec4Top = vec4(this.top[0], this.top[1], this.top[2], 1);
+            let look_direction = this.look_at.minus(this.position);
+            let rotationAxis = look_direction.cross(this.top).normalized();
+            let model_transform = Mat4.identity();
+            model_transform = model_transform.times(Mat4.translation(this.position[0], this.position[1], this.position[2]));
+            model_transform = model_transform.times(Mat4.rotation(-0.01, rotationAxis[0], rotationAxis[1], rotationAxis[2]));
+            model_transform = model_transform.times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
+            vec4LookAt = model_transform.times(vec4LookAt);
+            vec4Top = Mat4.rotation(-0.01, rotationAxis[0], rotationAxis[1], rotationAxis[2]).times(vec4Top);
+
+            this.look_at = vec3(vec4LookAt[0], vec4LookAt[1], vec4LookAt[2]);
+            this.top = vec3(vec4Top[0], vec4Top[1], vec4Top[2]);
             
         }
         if (this.direction[0] == 1){
             let temp = vec4(this.look_at[0], this.look_at[1], this.look_at[2], 1);
             let model_transform = Mat4.identity();
-            model_transform = model_transform.times(Mat4.translation(this.position[0], this.position[1], this.position[2])).times(Mat4.rotation(0.01, 0, 1, 0)).times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
-            console.log(model_transform);
+            model_transform = model_transform.times(Mat4.translation(this.position[0], this.position[1], this.position[2]));
+            model_transform = model_transform.times(Mat4.rotation(0.01, this.top[0], this.top[1], this.top[2]));
+            model_transform = model_transform.times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
+            //console.log(model_transform);
             temp = model_transform.times(temp);
-            console.log(temp);
+            //console.log(temp);
             this.look_at = vec3(temp[0], temp[1], temp[2]);
-            console.log(this.look_at);
+            //console.log(this.look_at);
         }
         if (this.direction[1] == 1){
             let temp = vec4(this.look_at[0], this.look_at[1], this.look_at[2], 1);
             let model_transform = Mat4.identity();
-            model_transform = model_transform.times(Mat4.translation(this.position[0], this.position[1], this.position[2])).times(Mat4.rotation(-0.01, 0, 1, 0)).times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
-            console.log(model_transform);
+            model_transform = model_transform.times(Mat4.translation(this.position[0], this.position[1], this.position[2]));
+            model_transform = model_transform.times(Mat4.rotation(-0.01, this.top[0], this.top[1], this.top[2]))
+            model_transform = model_transform.times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
+            //console.log(model_transform);
             temp = model_transform.times(temp);
-            console.log(temp);
+            //console.log(temp);
             this.look_at = vec3(temp[0], temp[1], temp[2]);
-            console.log(this.look_at);
+            //console.log(this.look_at);
         }
 
-        program_state.set_camera(Mat4.look_at(this.position, this.look_at, vec3(0, 1, 0)));
+        program_state.set_camera(Mat4.look_at(this.position, this.look_at, this.top));
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
-
-        // TODO: Create Planets (Requirement 1)
-        // this.shapes.[XXX].draw([XXX]) // <--example
 
         // TODO: Lighting (Requirement 2)
         const light_position = vec4(0, 5, 5, 1);
         // The parameters of the Light are: position, color, size
-        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000000000)];
 
         // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 3 and 4)
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         const yellow = hex_color("#fac91a");
+        const green = hex_color("#228B22");
 
         let model_transform = Mat4.identity();
 
@@ -164,6 +184,7 @@ export class Assignment3 extends Scene {
             let curr = this.blocks[i];
             this.shapes.block.draw(context, program_state, model_transform.times(Mat4.translation(curr[0], curr[1], curr[2])), this.materials.test.override({color: yellow}));
         }
+        this.shapes.floor.draw(context, program_state, model_transform.times(Mat4.rotation(Math.PI / 2, 1, 0, 0)).times(Mat4.scale(1000, 1000, 1)), this.materials.test.override({color: green}))
                 
        
         //this.shapes.block.draw(context, program_state, model_transform, this.materials.test.override({color: yellow}));
