25104b0bce63f6ebc904c38972f95f32498bed86
David Tung <dtung.725@gmail.com>
2021-11-17 16:22:50 -0800

    Implemented collision detection

--- movement.js
+++ movement.js
@@ -28,30 +28,39 @@ export class Constrained_Movement_Controls extends Scene {
         this.key_triggered_button("Look left", ["j"], () => this.direction[0] = 1, undefined, () => this.direction[0] = 0);
     }
     display(context, program_state) {
+
+        // Position and look_at after movement
+        let proposed_position = this.position;
+        let proposed_look_at = this.look_at;
+
+        // Move forward
         if (this.thrust[2] == 1){
             let look_direction = this.look_at.minus(this.position);
             look_direction = vec3(look_direction[0], 0, look_direction[2]).normalized();
-            this.position = this.position.plus(look_direction.times(0.5));
-            this.look_at = this.look_at.plus(look_direction.times(0.5));
+            proposed_position = this.position.plus(look_direction.times(0.5));
+            proposed_look_at = this.look_at.plus(look_direction.times(0.5));
         }
+        // Move backward
         if (this.thrust[3] == 1){
             let look_direction = this.look_at.minus(this.position);
             look_direction = vec3(look_direction[0], 0, look_direction[2]).normalized();
-            this.position = this.position.minus(look_direction.times(0.3));
-            this.look_at = this.look_at.minus(look_direction.times(0.3));
+            proposed_position = this.position.minus(look_direction.times(0.3));
+            proposed_look_at = this.look_at.minus(look_direction.times(0.3));
 
         }
+        // Move right
         if (this.thrust[1] == 1){
             let look_direction = this.look_at.minus(this.position);
             let right = look_direction.cross(vec3(0,1,0)).normalized();
-            this.position = this.position.plus(right.times(0.3));
-            this.look_at = this.look_at.plus(right.times(0.3));
+            proposed_position = this.position.plus(right.times(0.3));
+            proposed_look_at = this.look_at.plus(right.times(0.3));
         }
+        // Move left
         if (this.thrust[0] == 1) {
             let look_direction = this.look_at.minus(this.position);
             let right = look_direction.cross(vec3(0,1,0)).normalized();
-            this.position = this.position.minus(right.times(0.5));
-            this.look_at = this.look_at.minus(right.times(0.5));
+            proposed_position = this.position.minus(right.times(0.5));
+            proposed_look_at = this.look_at.minus(right.times(0.5));
         }
         if (this.direction[2] == 1) {
             let vec4LookAt = vec4(this.look_at[0], this.look_at[1], this.look_at[2], 1);
@@ -113,20 +122,29 @@ export class Constrained_Movement_Controls extends Scene {
             model_transform = model_transform.times(Mat4.translation(this.position[0] * -1, this.position[1] * -1, this.position[2] * -1));
             vec4LookAt = model_transform.times(vec4LookAt);
             this.look_at = vec3(vec4LookAt[0], vec4LookAt[1], vec4LookAt[2]);
-        }        
-        let posX = this.position[0];
-        let posY = this.position[1];
-        let posZ = this.position[2];
+        }
+
+        // Collision Detection Handling
+        let posX = proposed_position[0];
+        let posY = proposed_position[1];
+        let posZ = proposed_position[2];
         
+        let collide = false;
         for (let i = 0; i < this.blocks.length; i++) {
            let blockX = this.blocks[i][0];
            let blockY = this.blocks[i][1];
            let blockZ = this.blocks[i][2];
             
+            // Assume a bounding hitbox of 2 units
            if (blockX + 2 > posX && blockX < posX + 2 && blockY + 2 > posY && blockY < posY + 2 && blockZ + 2 > posZ && blockZ < posZ + 2) {
-                // collision detected assuming bounding hitbox of 2 units
-                console.log("yes");
+                collide = true;
+                break;
            }
+        }
+        // Update position and look_at
+        if (!collide) {
+            this.position = proposed_position;
+            this.look_at = proposed_look_at;
         }
         
 
